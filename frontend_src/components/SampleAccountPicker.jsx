8cc445439e5c96c2e47eb06f4b724419e5d4ff0a
noah <noah@jetbridge.com>
2019-11-06 17:08:35 -0600

    give sample account list items a hover state and a pointer cursor

--- frontend_src/components/SampleAccountPicker.jsx
+++ frontend_src/components/SampleAccountPicker.jsx
@@ -2,52 +2,11 @@ import React, { Component } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { TwitterUserImage } from './TwitterUserImage.jsx';
 
-// deprecated
-const AccountCard = (props) => {
-  return (
-      <Card>
-        <TwitterUserImage src={props.profileImgUrl} className="card-img-top" />
-        <Card.Body>
-            <div>
-                <Card.Title>{props.name}</Card.Title>
-            </div>
-            <div>
-                <Card.Text>Following {props.following} people</Card.Text>
-            </div>
-        </Card.Body>
-        <Card.Footer>
-            <Button 
-                className="sample-acct-btn"
-                variant="primary" 
-                onClick={() => props.clickHandler(props.username)}>
-                Try Account
-            </Button>
-        </Card.Footer>
-      </Card>
-  );
-};
-
-// deprecated
-const CardDeckAccountPicker = (props) => {
-  // based on CardDeck
-  return <div className="sample-acct-picker">
-      {props.accounts.map((account) => 
-          <AccountCard 
-              name={account.name} 
-              username={account.username} 
-              following={account.following} 
-              profileImgUrl={account.profile_img_url} 
-              clickHandler={props.optionClickHandler} 
-          />
-      )}
-  </div>
-};
-
 const SampleAccountListItem = (props) => {
-    const clickHandler = () => props.clickHandler(props.username)
+    const clickHandler = () => props.clickHandler(props.username);
 
     return (
-        <a className="d-flex align-items-center list-group-item list-group-item-action" onClick={clickHandler}>
+        <a className="d-flex align-items-center list-group-item list-group-item-action account-picker-list-item" onClick={clickHandler}>
             <div className="mr-2">
                 <TwitterUserImage 
                     src={props.profileImgUrl} 
@@ -87,6 +46,48 @@ const ListGroupSampleAccountPicker = (props) => {
             )}
         </div>
     );
+};
+
+
+// deprecated
+const AccountCard = (props) => {
+  return (
+      <Card>
+        <TwitterUserImage src={props.profileImgUrl} className="card-img-top" />
+        <Card.Body>
+            <div>
+                <Card.Title>{props.name}</Card.Title>
+            </div>
+            <div>
+                <Card.Text>Following {props.following} people</Card.Text>
+            </div>
+        </Card.Body>
+        <Card.Footer>
+            <Button 
+                className="sample-acct-btn"
+                variant="primary" 
+                onClick={() => props.clickHandler(props.username)}>
+                Try Account
+            </Button>
+        </Card.Footer>
+      </Card>
+  );
+};
+
+// deprecated
+const CardDeckAccountPicker = (props) => {
+  // based on CardDeck
+  return <div className="sample-acct-picker">
+      {props.accounts.map((account) => 
+          <AccountCard 
+              name={account.name} 
+              username={account.username} 
+              following={account.following} 
+              profileImgUrl={account.profile_img_url} 
+              clickHandler={props.optionClickHandler} 
+          />
+      )}
+  </div>
 };
 
 export { ListGroupSampleAccountPicker }
--- frontend_src/components/Slides.jsx
+++ frontend_src/components/Slides.jsx
@@ -19,8 +19,11 @@ const AtlanticTweetSlide = (props) => {
         authorIconUrl="/static/images/atlantic_icon.jpg"
       >
         <blockquote className="blockquote">
-            <p className="mb-0">
-                “Twitter is a highly individual experience that works like a collective <span className="hallucination">{ spanifyString("hallucination") }</span>, not a community. It’s probably totally fine that a good chunk of the nation’s elites spend so much time on it. What could go wrong?”
+            <p>
+                “Twitter, as it turns out, is not a good model of the world ... a highly individual experience that works like a collective <span className="hallucination">{ spanifyString("hallucination") }</span>, not a community.”
+            </p>
+            <p>
+              “It’s probably totally fine that a good chunk of the nation’s elites spend so much time on it.”
             </p>
             <footer className="blockquote-footer">
                 Alexis C. Madrigal <cite title="Twitter Is Not America"><a href="https://www.theatlantic.com/technology/archive/2019/04/twitter-is-not-america/587770/" target="_blank">“Twitter Is Not America”</a> The Atlantic</cite>
